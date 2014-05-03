90b511803224b75f5f27e200fea1eb29f9f912fb
yaboyanees <a.merzi@yahoo.com>
2014-05-02 21:43:12 -0500

    seed data removed

--- db/seeds.rb
+++ db/seeds.rb
@@ -11,12 +11,10 @@
 # Job.create(:name => "cook", :description => "description here", :training_id => "2")
 # Job.create(:name => "assistant", :description => "description here", :training_id => "3")
 # 
-Orientation.create(:orientation_date => "2014-03-22 08:00:00.000000")
-Orientation.create(:orientation_date => "2014-04-22 08:00:00.000000")
-Orientation.create(:orientation_date => "2014-05-22 08:00:00.000000")
-# 
-Training.create(:name => "computer training", :certification_date => "2014-03-22")
-Training.create(:name => "food handler course", :certification_date => "2014-04-22")
-Training.create(:name => "no training", :certification_date => "2014-05-22")
-
+# Orientation.create(:orientation_date => "2014-03-22 08:00:00.000000")
+# Orientation.create(:orientation_date => "2014-04-22 08:00:00.000000")
+# Orientation.create(:orientation_date => "2014-05-22 08:00:00.000000")
+# Training.create(:name => "computer training", :certification_date => "2014-03-22")
+# Training.create(:name => "food handler course", :certification_date => "2014-04-22")
+# Training.create(:name => "no training", :certification_date => "2014-05-22")
 User.create(:email => "admin@ops.com", :password => "volunteer", :first_name => "Anees", :last_name => "Merzi", :phone_number => "254-595-5555", :admin => "t" )
